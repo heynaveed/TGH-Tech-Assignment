c775be1a9ed7d25401907fa9146bb1da4009f0df
heynaveed <naveedpasha0058@gmail.com>
2023-04-15 15:06:20 +0530

    Basic SetUp done

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Home(){
+    return(
+        <>
+        <Box border={"1px solid black"}>
+            <Text>Home Page</Text>
+        </Box>
+        </>
+    )
+}
---
+++ src/Redux/Reducer.js
@@ -0,0 +1,15 @@
+const initialState={
+    isLoding:false,
+    isError:false,
+    Bookmark:[]
+}
+
+const Reducer=(State=initialState,action)=>{
+    const {type,payload}=action;
+    switch(type){
+        default:
+            return State
+    }
+}
+
+export{Reducer}
